a940bb24f554678775c773be1fcbdf483d73bea3
technovahub@2025 <technovahubcareer@gmail.com>
2025-12-30 12:17:01 +0530

    updated PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -209,7 +209,7 @@ class PageController extends Controller
         $validated = $request->validate([
             'title' => 'required|string|max:255',
             'code' => 'required',
-            'url' => 'nullable|url',
+            'url' => ['nullable', 'regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/'],
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:5048',
         ]);
 
